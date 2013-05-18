a7ef0d683f3d20b626235355a883e5dcc93d826f
Dray <hnzksq@gmail.com>
2013-05-18 22:23:55 +0800

    t22

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -13,11 +13,11 @@ if ( !empty($_SESSION['access_token']['oauth_token']) && !empty($_SESSION['acces
     
     $result = $twitteroauth->get('users/lookup', array('screen_name' => $username));
     //echo '<pre>',var_dump($result,true),'</pre>',die();;
-    $twitter = @$result[0];
-    if(empty($twitter)){
+    if(empty($result[0])){
 	    session_destroy();
 	    header('Location: /index.php');
     }
+    $twitter = @$result[0];
 
 	$setp = isset($_GET["setp"]) ? $_GET["setp"] : '-1';
 	if ( $setp == "0" ) {
